660561a43fa20724146c7085f8bab5498391a07b
dnyscientist <dannyprabowowo@gmail.com>
2024-06-28 13:47:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,3 +79,31 @@ def main() :
     st.write('Luas dari segitiga adalah {}'.format(1/2*alas_input*tinggi_input))
 if __name__ == '__main__' : 
   main()
+
+def main() : 
+    #sidebar 
+    sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+    sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
+    
+    #columns :
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+       st.header("A cat")
+       st.image("https://static.streamlit.io/examples/cat.jpg")
+    #atau dengan assignment 
+    image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+    with col2:
+       st.header("A dog")
+       st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    with col3:
+       st.header("An owl")
+       st.image("https://static.streamlit.io/examples/owl.jpg")
+    #expander 
+    #dengan with atau dengan assignment 
+    expander = st.expander("Klik Untuk Detail ")
+    expander.write('Anda Telah Membuka Detail')
+if __name__ == '__main__' : 
+  main()
