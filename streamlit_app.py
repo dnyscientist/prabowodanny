5b393b54fc98ec7e63e7d1f3a05c5d3e17b4bde5
dnyscientist <dannyprabowowo@gmail.com>
2024-06-28 11:04:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,20 +19,20 @@ def main() :
 if __name__ == '__main__' : 
   main()
 
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
+house = pd.read_csv('house_clean.csv')
 #read json file dari data covid 
-flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
-  "month_limit": "2020-01-01",
-  "window_size": 12
-}).json()
+# flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
+#   "month_limit": "2020-01-01",
+#   "window_size": 12
+# }).json()
 
 def main() : 
   st.write('Contoh dataframe')
   st.dataframe(titanic)
-  st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  # st.write('Contoh JSON')
+  # st.json(flight_passanger_api)
+  # st.write('Metrics')
+  # st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
   AgGrid(titanic)
   st.table([x for x in range(1,5)])
