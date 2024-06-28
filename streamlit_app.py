29ce36df63fe12b369ef7cfdf6b1c19361755d1b
dnyscientist <dannyprabowowo@gmail.com>
2024-06-28 10:03:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,18 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-def main() : 
+def main_1() : 
   st.write('Minimal Example')
 
-if __name__ == '__main__' : 
-  main()
+if __name__ == '__main_1__' : 
+  main_1()
   
-def main() : 
+def main_2() : 
   st.header('This is Header')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
 
-if __name__ == '__main__' : 
-  main()
+if __name__ == '__main_2__' : 
+  main_2()
