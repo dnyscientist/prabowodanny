d4ab14e53ea34de07fc4f2e6f7443dcc5314029f
dnyscientist <dannyprabowowo@gmail.com>
2024-06-28 13:59:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,3 +115,19 @@ def main() :
     expander.write('Anda Telah Membuka Detail')
 if __name__ == '__main__' : 
   main()
+
+def main() : 
+    #sidebar 
+    with st.form("Data Diri"):
+       st.write("Inside the form")
+       slider_val = st.slider("Form slider")
+       checkbox_val = st.checkbox("Form checkbox")
+
+       # Every form must have a submit button.
+       submitted = st.form_submit_button("Submit")
+       if submitted:
+           st.write("slider", slider_val, "checkbox", checkbox_val)
+
+    st.write("Outside the form")
+if __name__ == '__main__' : 
+  main()
