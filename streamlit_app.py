d11dc674b31371bf6be8c8cdfbbc83dcfb4387da
dnyscientist <dannyprabowowo@gmail.com>
2024-06-28 13:25:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,17 @@ from st_aggrid import AgGrid
 # Read the CSV file
 house = pd.read_csv('house_clean.csv')
 
-# Read JSON file from GitHub repository
-url = 'timeseries.json'
-response = requests.get(url)
-covid = response.json()
+# # Read JSON file from GitHub repository
+# url = 'timeseries.json'
+# response = requests.get(url)
+# covid = response.json()
 
 def main():
     st.write('Contoh dataframe')
     st.dataframe(house)
     
-    st.write('Contoh JSON')
-    st.json(covid)
+    # st.write('Contoh JSON')
+    # st.json(covid)
     
    
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
@@ -43,7 +43,7 @@ def main():
     # st.write('Menampilkan Dataframe dengan St AgGrid')
     # AgGrid(house)
     
-    # st.table([x for x in range(1, 5)])
+    st.table([x for x in range(1, 5)])
 
 if __name__ == '__main__':
     main()
